43d55df2649e237141823e4ffd91859f6b75adf8
Mihai Manole <mesterum@users.noreply.github.com>
2023-11-30 22:52:35 +0000

    2206 2207

--- libs/solver/solver/src/lib/2022/Day06.ts
+++ libs/solver/solver/src/lib/2022/Day06.ts
@@ -1,4 +1,14 @@
 import { input, output, part } from '../solver';
 
-part === 1 && output('Part 1');
-part === 2 && output('Part 2');
+const distinctLength = part === 2 ? 14 : 4;
+let [first, last] = [0, 1];
+for (; last - first < distinctLength && last < input.length; last++) {
+  const checkedChar = input.charCodeAt(last);
+  for (let i = last - 1; i >= first; i--)
+    if (checkedChar == input.charCodeAt(i)) {
+      first = i + 1;
+      break;
+    }
+}
+
+output(last);
--- libs/solver/solver/src/lib/2022/Day07.ts
+++ libs/solver/solver/src/lib/2022/Day07.ts
@@ -1,4 +1,71 @@
 import { input, output, part } from '../solver';
 
-part === 1 && output('Part 1');
-part === 2 && output('Part 2');
+let result = 0;
+const iter = input.split('\n').values(),
+  rootDir = Object.assign(new Map(), { sizeInBytes: 0, unevaledDirs: 0, evaled: false }),
+  workingDirs = [rootDir],
+  evaledDirs = [],
+  processEvaledDirs = (dir: typeof workingDir) => {
+    const dirSize = dir.sizeInBytes;
+    if (part === 2) evaledDirs.push(dir);
+    else if (dirSize < 100_000) result += dirSize;
+    dir.evaled = true;
+    return dirSize;
+  },
+  closeLsMode = () => {
+    if (workingDir.unevaledDirs == 0) {
+      let dirSize = processEvaledDirs(workingDir);
+      for (let i = workingDirs.length - 2; i >= 0; i--) {
+        const wd = workingDirs[i];
+        wd.unevaledDirs--;
+        wd.sizeInBytes += dirSize;
+        if (wd.unevaledDirs > 0) break;
+        dirSize = processEvaledDirs(wd);
+      }
+    }
+    lsMode = false;
+  };
+let workingDir = rootDir,
+  lsMode = false;
+for (const line of iter) {
+  if (line == '') continue;
+  if (line.startsWith('$')) {
+    if (lsMode) closeLsMode();
+    const command = line.match(/^\$ *(cd|ls) *(.*)/);
+    if (command[1] == 'ls') {
+      lsMode = true;
+      continue;
+    }
+    switch (command[2]) {
+      case '/':
+        workingDirs.length = 1;
+        workingDir = rootDir;
+        break;
+      case '..':
+        workingDirs.pop();
+        workingDir = workingDirs.at(-1);
+        break;
+      default:
+        workingDir = workingDir.get(command[2]);
+        if (!workingDir) throw new Error('dir ${command[2]} not found');
+        if (workingDir.evaled) throw new Error('dir ${command[2]} was fully scanned');
+        workingDirs.push(workingDir);
+        break;
+    }
+    continue;
+  }
+  const dirItems = line.match(/^(dir|\d+) (.+)/);
+  if (dirItems[1] == 'dir') {
+    const dir = Object.assign(new Map(), { sizeInBytes: 0, unevaledDirs: 0, evaled: false });
+    workingDir.set(dirItems[2], dir);
+    workingDir.unevaledDirs++;
+    continue;
+  }
+  const fileSize = +dirItems[1];
+  if (isNaN(fileSize)) throw new Error(`file size ${dirItems[1]} is NaN`);
+  workingDir.sizeInBytes += fileSize;
+}
+if (lsMode) closeLsMode();
+if (part === 1) output(result);
+const threshold = rootDir.sizeInBytes - 40_000_000;
+output(evaledDirs.reduce((min, { sizeInBytes: s }) => (s < threshold ? min : s < min ? s : min), rootDir.sizeInBytes));
