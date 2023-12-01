3773a703c44f093e76bd3b4bd3cdffe25cb08445
Mihai Manole <mesterum@users.noreply.github.com>
2023-12-01 09:38:13 +0000

    2301 Lookahead assertion

--- libs/solver/solver/src/lib/2023/Day01.ts
+++ libs/solver/solver/src/lib/2023/Day01.ts
@@ -3,23 +3,18 @@ import { input, output, part } from '../solver';
 // part === 2 && output('Part 2');
 
 const iter = input.split('\n').values(),
-  matchExp = part == 2 ? /one|two|three|four|five|six|seven|eight|nine|\d/g : /\d/g,
+  matchExp = part == 1 ? /\d/g : /on(?=e)|tw(?=o)|th(?=ree)|fo(?=ur)|fi(?=ve)|si(?=x)|se(?=ven)|ei(?=ght)|ni(?=ne)|\d/g,
   // eslint-disable-next-line no-sparse-arrays
-  words = [, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  words = [, 'on', 'tw', 'th', 'fo', 'fi', 'si', 'se', 'ei', 'ni'];
 let sum = 0;
 for (const line of iter) {
   if (line == '') continue;
-  const numbers = line.match(matchExp);
-  // eslint-disable-next-line no-autofix/prefer-const
-  let [first, last] = [numbers[0], numbers.at(-1)];
+  const numbers = line.match(matchExp),
+    [first, last] = [numbers[0], numbers.at(-1)];
   // output(numbers);
   if (part === 1) {
     sum += +(first + last);
     continue;
-  }
-  for (let i = line.length - 4; i >= 0; i--) {
-    last = line.slice(i).match(matchExp)?.at(-1);
-    if (last) break;
   }
   sum += [first, last].reduce((p, v) => p * 10 + (isNaN(+v) ? words.indexOf(v) : +v), 0);
 }
