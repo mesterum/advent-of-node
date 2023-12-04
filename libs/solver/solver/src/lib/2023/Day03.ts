72890ee1586f56543738ba7e7f9efa04edf1544a
Mihai Manole <mesterum@users.noreply.github.com>
2023-12-04 17:44:03 +0000

    2304

--- libs/solver/solver/src/lib/2023/Day03.ts
+++ libs/solver/solver/src/lib/2023/Day03.ts
@@ -14,78 +14,67 @@ const iter = input.split('\n').values(),
         line.matchAll(partNoExp), //partNoCrt
         line.matchAll(symbolExp), //symbolCrt
       ],
-      itersRes = iters.map(i => i.next()), // as IteratorResult<RegExpMatchArray, never>[],
+      itersRes = iters.map(i => i.next()) as IteratorResult<RegExpMatchArray, RegExpMatchArray>[],
       itersIndex = itersRes.map(({ value: v, done }, iter) => ({ iter, index: done ? -1 : v.index })).filter(v => v.index >= 0);
     let activeIters = itersRes.reduceRight((p, { done }) => (p << 1) + (done ? 0 : 1), 0);
     while (itersIndex.length > 0) {
       const iterWMinIndex = itersIndex.sort((p, c) => p.index - c.index)[0],
-        { value } = itersRes[iterWMinIndex.iter];
+        { iter, index } = iterWMinIndex,
+        { value } = itersRes[iter];
       let partNo: string; //, symbolIdx=0
-      switch (iterWMinIndex.iter) {
+      switch (iter) {
         case 0: //partNoPrev
           partNo = value[0];
-          if (8 & activeIters && itersRes[3].value.index <= iterWMinIndex.index + partNo.length) {
+          if (8 & activeIters && itersRes[3].value.index <= index + partNo.length) {
             if (itersRes[3].value[0] == '*') itersRes[3].value.push(partNo);
             value[1] = 'f';
           }
           if (part == 1 && value[1] == 'f') partNosFound.push(+partNo);
-          if ((itersRes[0] = iters[0].next()).done) {
-            activeIters &= 15 - 1;
-            itersIndex.shift();
-          } else iterWMinIndex.index = itersRes[0].value.index;
           break;
         case 1: //symbolPrev
-          if (4 & activeIters && itersRes[2].value.index <= iterWMinIndex.index + 1) {
+          if (4 & activeIters && itersRes[2].value.index <= index + 1) {
             if (value[0] == '*') value.push(itersRes[2].value[0]);
             itersRes[2].value[1] = 'f';
           }
-          if (part == 2 && value[0] == '*' && value.length == 3) partNosFound.push(value[1] * value[2]);
-          if ((itersRes[1] = iters[1].next()).done) {
-            activeIters &= 15 - 2;
-            itersIndex.shift();
-          } else iterWMinIndex.index = itersRes[1].value.index;
+          if (part == 2 && value[0] == '*' && value.length == 3) partNosFound.push(+value[1] * +value[2]);
           break;
         case 2: //partNoCrt
           partNo = value[0];
-          if (8 & activeIters && itersRes[3].value.index == iterWMinIndex.index + partNo.length) {
+          if (8 & activeIters && itersRes[3].value.index == index + partNo.length) {
             if (itersRes[3].value[0] == '*') itersRes[3].value.push(partNo);
             value[1] = 'f';
           }
-          if (2 & activeIters && itersRes[1].value.index <= iterWMinIndex.index + partNo.length) {
+          if (2 & activeIters && itersRes[1].value.index <= index + partNo.length) {
             if (itersRes[1].value[0] == '*') itersRes[1].value.push(partNo);
             value[1] = 'f';
           }
           partNoPrev.push(value);
-          if ((itersRes[2] = iters[2].next()).done) {
-            activeIters &= 15 - 4;
-            itersIndex.shift();
-          } else iterWMinIndex.index = itersRes[2].value.index;
           break;
         case 3: //symbolCrt
-          if (4 & activeIters && itersRes[2].value.index == iterWMinIndex.index + 1) {
+          if (4 & activeIters && itersRes[2].value.index == index + 1) {
             if (value[0] == '*') value.push(itersRes[2].value[0]);
             itersRes[2].value[1] = 'f';
           }
-          if (1 & activeIters && itersRes[0].value.index <= iterWMinIndex.index + 1) {
+          if (1 & activeIters && itersRes[0].value.index <= index + 1) {
             if (value[0] == '*') value.push(itersRes[0].value[0]);
             itersRes[0].value[1] = 'f';
           }
           symbolPrev.push(value);
-          if ((itersRes[3] = iters[3].next()).done) {
-            activeIters &= 15 - 8;
-            itersIndex.shift();
-          } else iterWMinIndex.index = itersRes[3].value.index;
           break;
         default:
           break;
       }
+      if ((itersRes[iter] = iters[iter].next()).done) {
+        activeIters &= 15 - (1 << iter);
+        itersIndex.shift();
+      } else iterWMinIndex.index = itersRes[iter].value.index;
     }
     prev[0] = partNoPrev;
     prev[1] = symbolPrev;
     return partNosFound;
   };
 let sum = 0;
-const prevPartNo_symbol = [[], []] as RegExpMatchArray[][];
+const prevPartNo_symbol = [[], []] as PrevLine;
 for (const line of iter) {
   // if (line == '') continue;
   const partNosFound = parseLine(line, prevPartNo_symbol);
--- libs/solver/solver/src/lib/2023/Day04.ts
+++ libs/solver/solver/src/lib/2023/Day04.ts
@@ -1,4 +1,30 @@
 import { input, output, part } from '../solver';
 
-part === 1 && output('Part 1');
-part === 2 && output('Part 2');
+const iter = input.split('\n'),
+  cardRExp = /^Card (\d+): */y,
+  allCards = Array.from(iter, () => 0);
+let totalPoints = 0,
+  cardIdx = 0;
+for (const line of iter) {
+  if (line == '') continue;
+  line.match(cardRExp);
+  const numbers = line.slice(cardRExp.lastIndex).split(/ +/),
+    separator = numbers.indexOf('|'),
+    thatIHaveNos = numbers.slice(separator + 1);
+  let points = 0;
+  for (const winningNo of numbers) {
+    if (winningNo == '|') break;
+    if (thatIHaveNos.includes(winningNo)) points++;
+  }
+  cardRExp.lastIndex = 0;
+  if (part == 1) {
+    if (points--) totalPoints += 1 << points;
+    continue;
+  }
+  const cards = ++allCards[cardIdx];
+  for (let i = cardIdx + 1; points && i < allCards.length; points--, i++) allCards[i] += cards;
+  cardIdx++;
+}
+
+part === 1 && output(totalPoints);
+part === 2 && output(allCards.reduce((s, v) => s + v, 0));
