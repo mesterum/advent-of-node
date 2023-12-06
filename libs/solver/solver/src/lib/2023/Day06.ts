2a6f3a7f706a3fa857436d4b04b2bfca09410ee0
Mihai Manole <mesterum@users.noreply.github.com>
2023-12-06 09:52:49 +0000

    2306

--- libs/solver/solver/src/lib/2023/Day06.ts
+++ libs/solver/solver/src/lib/2023/Day06.ts
@@ -1,4 +1,18 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { input, output, part } from '../solver';
 
-part === 1 && output('Part 1');
-part === 2 && output('Part 2');
+const lines = input.split('\n'),
+  noWays2BeatRec = (time: number, dist: number) => {
+    const delta = time * time - 4 * dist,
+      max = Math.floor((time + Math.sqrt(delta - 1)) / 2);
+    return 2 * max - time + 1;
+  };
+if (part === 1) {
+  const times = lines[0].match(/\d+/g)!.map(v => +v),
+    distances = lines[1].match(/\d+/g)!.map(v => +v);
+  output(times.map((time, idx) => noWays2BeatRec(time, distances[idx])).reduce((p, v) => p * v, 1));
+} else {
+  const time = +lines[0].match(/\d+/g)!.join(''),
+    distance = +lines[1].match(/\d+/g)!.join('');
+  output(noWays2BeatRec(time, distance));
+}
