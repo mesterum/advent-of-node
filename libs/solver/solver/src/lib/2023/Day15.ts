56271104aae2fe3e1ff7580b282491c1a3b4cc89
Mihai Manole <mesterum@users.noreply.github.com>
2023-12-16 19:02:49 +0000

    2315

--- libs/solver/solver/src/lib/2023/Day15.ts
+++ libs/solver/solver/src/lib/2023/Day15.ts
@@ -1,4 +1,33 @@
 import { input, output, part } from '../solver';
 
-part === 1 && output('Part 1');
-part === 2 && output('Part 2');
+const iter = input.split(/,|\n/),
+  hash = (line: string) => {
+    let crtValue = 0
+    for (let idx = 0; idx < line.length; idx++)
+      crtValue = (crtValue + line.charCodeAt(idx)) * 17 & 0xFF
+    return crtValue
+  },
+  stepReX = /^(\w+)(-|=\d)/,
+  lenses = new Map<string, number>(),
+  lenses7Box = new Uint16Array(256)
+
+let sum = 0
+for (const line of iter) {
+  if (line == "") continue
+  if (part === 1) {
+    sum += hash(line); continue
+  }
+  const parse = stepReX.exec(line)
+  if (!parse) throw Error(`"${line}" is not a valid step`)
+  if (parse[2] == '-') lenses.delete(parse[1])
+  else lenses.set(parse[1], +parse[2].charAt(1))
+}
+
+part === 1 && output(sum);
+
+lenses.forEach((focalLength, label) => {
+  const box = hash(label)
+  sum += (box + 1) * ++lenses7Box[box] * focalLength
+})
+
+output(sum);
